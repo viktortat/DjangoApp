31a19a73b119b7ec73583b8eb80579bdd798f004
Виктор <viktortat@gmail.com>
2017-08-27 18:34:03 +0300

    3

--- DjangoApp/views.py
+++ DjangoApp/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+
+def home(request):
+    content = {'text':'Привет мир!','title':'Главная страница'}
+    return render(request,'base.html',content)
